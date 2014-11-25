4f88b5d6bf234595836188ab5f2e53a6f0800038
Roman Gafiyatullin <r.gafiyatullin@me.com>
2014-11-26 01:18:20 +0300

    Added shell record definitions for result-types.

--- include/orca.hrl
+++ include/orca.hrl
@@ -8,12 +8,14 @@
 		number_of_warnings = 0 :: non_neg_integer(),
 		msg = <<>> :: binary()
 	}).
+
 -record(orca_rows, {
 		rows = [] :: [ [term()] ],
 		field_defs = [] :: [ [term()] ],
-		status_flags :: non_neg_integer(),
-		number_of_warnings :: non_neg_integer()
+		status_flags = 0 :: non_neg_integer(),
+		number_of_warnings = 0 :: non_neg_integer()
 	}).
+
 -record(orca_error, {
 		code = 0 :: non_neg_integer(),
 		msg = <<>> :: binary(),
@@ -21,7 +23,12 @@
 		sql_state = <<>> :: binary()
 	}).
 -record(orca_request_local_file_content, {
-		filename :: binary()
+		filename = <<>> :: binary()
 	}).
+
+% rd(orca_ok, { affected_rows = 0, last_inserted_id = 0, status_flags = 0, number_of_warnings = 0, msg = <<>> }).
+% rd(orca_rows, { rows = [], field_defs = [], status_flags = 0, number_of_warnings = 0 }).
+% rd(orca_error, { code = 0, msg = <<>>, sql_state_marker = <<>>, sql_state = <<>> }).
+% rd(orca_request_local_file_content, { filename = <<>> }).
 
 -endif. % orca_include_orca_hrl
