f26e3cd1784a4b42c8cddbc748ac8f36179d55d9
Roman Gafiyatullin <r.gafiyatullin@me.com>
2016-02-05 13:27:26 +0300

    orca_url:render/1 added

--- src/orca_url.erl
+++ src/orca_url.erl
@@ -1,11 +1,13 @@
 -module (orca_url).
--export ([parse/1]).
+-export ([parse/1, render/1]).
+
+-define(default_mysqld_port, 3306).
 
 parse( Url ) when is_binary( Url ) -> parse( binary_to_list( Url ) );
 parse( Url ) when is_list( Url ) ->
 	try
 		{ok, {mysql, UserPassword, Host, Port, [ $/ | Database ], MaybeQueryString}} =
-			http_uri:parse(Url, [{scheme_defaults, [{mysql, 3306}]}]),
+			http_uri:parse(Url, [{scheme_defaults, [{mysql, ?default_mysqld_port}]}]),
 		QueryString =
 			case MaybeQueryString of
 				[ $? | QS ] -> QS;
@@ -29,3 +31,21 @@ parse( Url ) when is_list( Url ) ->
 	catch
 		Error:Reason -> {Error, Reason}
 	end.
+
+render(Props) ->
+	User = proplists:get_value(user, Props),
+	Password = proplists:get_value(password, Props),
+	Host = proplists:get_value(host, Props),
+	Port = proplists:get_value(port, Props, ?default_mysqld_port),
+	DbName = proplists:get_value(db_name, Props),
+	PoolSize = proplists:get_value(pool_size, Props, 1),
+	MinRestartInterval = proplists:get_value(min_restart_interval, Props, 1000),
+
+	iolist_to_binary([
+		<<"mysql://">>,
+		User, $:, Password, $@,
+		Host, $:, integer_to_list(Port), $/, DbName, $?,
+		<<"pool_size=">>, integer_to_list(PoolSize),
+		<<"&min_restart_interval=">>, integer_to_list(MinRestartInterval)
+	]).
+
